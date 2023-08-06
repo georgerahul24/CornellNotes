0631259d43cc74729176607f49e87bd6f60dfe53
George Rahul <75750164+georgerahul24@users.noreply.github.com>
2023-08-07 01:47:54 +0530

    Adding Users to the sign Up page works

--- SignUp/views.py
+++ SignUp/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.views import View
 from django.shortcuts import render
-from django.template import loader
 from .forms import SignUpForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 
 class SignUp(View):
@@ -12,4 +13,25 @@ class SignUp(View):
         return render(request, 'SignUpPage.html', {'f': form})
 
     def post(self, request):
-        pass
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(form.cleaned_data["name"], form.cleaned_data["email"],
+                                     form.cleaned_data["password"])
+
+            user = authenticate(request, username=form.cleaned_data["name"], password=form.cleaned_data["password"])
+            if user is not None:
+                login(request, user)
+            else:
+                return HttpResponse("Error")
+
+            return HttpResponse(
+                f'{form.cleaned_data["name"]}, {form.cleaned_data["email"]}, {form.cleaned_data["password"]}')
+
+
+
+
+
+        else:
+
+            field_errors = [(field.label, field.errors) for field in form]
+            return HttpResponse(f"Invalid Form {field_errors}")
